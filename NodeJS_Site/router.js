74073249f63a7ea85853ea02a3781536e71169ea
danielbutler <cactusfly@gmail.com>
2016-10-06 19:18:55 -0400

    Add NodeJS_Site feature

---
+++ NodeJS_Site/router.js
@@ -0,0 +1,57 @@
+var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
+
+// Handle HTTP route GET / and POST / i.e. home
+function home(request, response) {
+  // if url == "/" && GET
+  if(request.url === "/") {
+    // show search
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    renderer.view("header", {}, response);
+    renderer.view("search", {}, response);
+    renderer.view("footer", {}, response);
+  }
+  
+  // if url == "/" && POST
+    // redirect to /:username
+}
+
+// Handle HTTP route GEt /:username i.e. /chalkers
+function user(request, response) {
+  // if url == "/..."
+  var username = request.url.replace("/", "");
+  if(username.length > 0) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    renderer.view("header", {}, response);
+    
+    // get json from Treehouse
+    var studentProfile = new Profile(username);
+    // on "end" 
+    studentProfile.on("end", function(profileJSON) {
+        // show profile
+      
+      // Store the values which we need
+      var values = {
+        avatarURL: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javaScriptPoints: profileJSON.points.JavaScript
+      };
+      // Simple Response
+      renderer.view("profile", values, response);
+      renderer.view("footer", {}, response);
+    });
+    
+    // on "error"
+    studentProfile.on("error", function(error){
+      // show error
+      renderer.view("error", {errorMessage: error.message}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+    });
+    
+  }
+}
+
+module.exports.home = home;
+module.exports.user = user;
